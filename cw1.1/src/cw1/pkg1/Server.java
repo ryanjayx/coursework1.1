42ed073005fd7430fb1274ae801c829c5bab1aa4
M00608315 <M00608315@kvmdxlab110pc23.studentmdx.ac>
2017-11-21 13:38:44 +0400

    Working to add server class

---
+++ cw1.1/src/cw1/pkg1/Server.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cw1.pkg1;
+
+/**
+ *
+ * @author M00608315
+ */
+public class Server {
+    public ThreeDShapes(String name, int noOfSides) {
+        super(name, noOfSides);
+    }
+    
+    public abstract double getSurfaceArea();
+    
+    public abstract double getVolume();
+}
+    
+    
+}
